b7f7747528328e529d6a9a5cdef10f7e31038d14
student777 <darkdakku@gmail.com>
2021-01-31 18:03:05 +0900

    Add config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,14 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "rules": {
-    },
-    "extends": [
-        "eslint:recommended",
-        "airbnb-base",
-        "prettier"
-    ]
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": "off",
+  },
+  extends: ["eslint:recommended", "airbnb-base", "prettier"],
 };
