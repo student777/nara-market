5198ea8293f08ba7a285c658c67e8c99a18f076f
student777 <darkdakku@gmail.com>
2021-02-22 01:21:01 +0900

    Refactor tbid

--- tbid.js
+++ tbid.js
@@ -3,8 +3,20 @@ import iconv from "iconv-lite";
 import { JSDOM } from "jsdom";
 // eslint-disable-next-line import/extensions
 import reg from "./keywords.js";
+import { from, to } from "./fromto.js";
 
-export default async function tbid(from, to) {
+export default async function tbid() {
+  const searched = await parseTable();
+  await Promise.all(
+    searched.map(async (s) => {
+      const price = await parseDetail(s.num);
+      s.price = price;
+    })
+  );
+  return searched;
+}
+
+async function parseTable(page) {
   const listUrl = `http://www.g2b.go.kr:8101/ep/tbid/tbidList.do?searchType=1&taskClCds=5&searchDtType=1&fromBidDt=${from}&toBidDt=${to}&recordCountPerPage=100`;
   const { data } = await axios.get(listUrl, {
     responseType: "arraybuffer",
@@ -28,24 +40,23 @@ export default async function tbid(from, to) {
       });
     }
   });
-  await Promise.all(
-    searched.map(async (s) => {
-      const [bidno, bidseq] = s.num.split("-");
-      const detailUrl = `http://www.g2b.go.kr:8081/ep/invitation/publish/bidInfoDtl.do?bidno=${bidno}&bidseq=${bidseq}&releaseYn=Y&taskClCd=5`;
-      const detailPage = await axios.get(detailUrl + s.num, {
-        responseType: "arraybuffer",
-      });
-      const domDetail = new JSDOM(iconv.decode(detailPage.data, "EUC-KR"));
-      try {
-        const price = domDetail.window.document
-          .querySelectorAll("div.section > table.table_info")[3]
-          .querySelector("tbody").children[1].children[3];
-        const dd = s;
-        dd.price = price.textContent.trim();
-      } catch {
-        console.log(`[ERROR] bid in ${detailUrl}`);
-      }
-    })
-  );
   return searched;
+}
+
+async function parseDetail(num) {
+  const [bidno, bidseq] = num.split("-");
+  const detailUrl = `http://www.g2b.go.kr:8081/ep/invitation/publish/bidInfoDtl.do?bidno=${bidno}&bidseq=${bidseq}&releaseYn=Y&taskClCd=5`;
+  const detailPage = await axios.get(detailUrl + num, {
+    responseType: "arraybuffer",
+  });
+  const domDetail = new JSDOM(iconv.decode(detailPage.data, "EUC-KR"));
+  try {
+    const price = domDetail.window.document
+      .querySelectorAll("div.section > table.table_info")[3]
+      .querySelector("tbody").children[1].children[3];
+    return price.textContent.trim();
+  } catch {
+    console.error(`[ERROR] bid in ${detailUrl}`);
+    return "[ERROR]";
+  }
 }
