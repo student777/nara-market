d117eeba9389e292c716dd3f6b537e7195576d84
student777 <darkdakku@gmail.com>
2021-02-22 12:55:08 +0900

    Search N page from tbid

--- tbid.js
+++ tbid.js
@@ -5,8 +5,17 @@ import { JSDOM } from "jsdom";
 import reg from "./keywords.js";
 import { from, to } from "./fromto.js";
 
+const countPerPage = 100;
+
 export default async function tbid() {
-  const searched = await parseTable();
+  const count = await checkTotalCount();
+  const endPage =  Math.ceil(count / countPerPage);
+  const searched = [];
+  await Promise.all(
+    Array.from(Array(endPage), (_, index) => index + 1).map(async (pageNum) => {
+      (await parseTable(pageNum)).forEach(d => searched.push(d));
+    })
+  );
   await Promise.all(
     searched.map(async (s) => {
       const price = await parseDetail(s.num);
@@ -16,8 +25,18 @@ export default async function tbid() {
   return searched;
 }
 
+async function checkTotalCount() {
+  const url = `http://www.g2b.go.kr:8101/ep/tbid/tbidList.do?searchType=1&taskClCds=5&searchDtType=1&fromBidDt=${from}&toBidDt=${to}&useTotalCount=Y`;
+  const { data } = await axios.get(url, {
+    responseType: "arraybuffer",
+  });
+  const dom = new JSDOM(iconv.decode(data, "EUC-KR"));
+  const totalCount = dom.window.document.querySelector("div.inforight > span.page").textContent.match(/\d+/)[0];
+  return totalCount;
+}
+
 async function parseTable(page) {
-  const listUrl = `http://www.g2b.go.kr:8101/ep/tbid/tbidList.do?searchType=1&taskClCds=5&searchDtType=1&fromBidDt=${from}&toBidDt=${to}&recordCountPerPage=100`;
+  const listUrl = `http://www.g2b.go.kr:8101/ep/tbid/tbidList.do?searchType=1&taskClCds=5&searchDtType=1&fromBidDt=${from}&toBidDt=${to}&recordCountPerPage=${countPerPage}&currentPageNo=${page}`;
   const { data } = await axios.get(listUrl, {
     responseType: "arraybuffer",
   });
