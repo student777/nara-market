2700915586696cff717e400341e543735a094c80
student777 <darkdakku@gmail.com>
2021-03-01 19:39:25 +0900

    Refactor: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,9 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
+    node: true,
   },
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module",
   },
   rules: {
     "no-console": "off",
